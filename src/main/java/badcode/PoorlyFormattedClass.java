a8be9a348523c031117b8967a03598f6ce229cf8
pramodkumaryadav <pramodyadav027@gmail.com>
2023-03-04 15:14:48 +0100

    initial commit. Adding framework support a java class and a gitignore file.

---
+++ src/main/java/badcode/PoorlyFormattedClass.java
@@ -0,0 +1,13 @@
+package badcode;
+public
+class
+PoorlyFormattedClass{
+    public static void main(String[] args)
+    {
+        System.out.println("This is a poorly formatted class");
+    }
+
+    @Override
+    public String toString(){
+        System.out.println("Poorly formatted toString method"); return "PoorlyFormattedClass{}";
+    }}
